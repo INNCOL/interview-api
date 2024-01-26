532b4c9dd9ced9a83447df1be8e0dd203176cc5e
Jose Alvarez <alvarez_3993@hotmail.com>
2024-01-25 18:22:02 -0600

    fix(Header): type

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { setupSwagger } from './swagger/swagger';
 import helmet from 'helmet';
 import { get } from 'http';
 import { createWriteStream } from 'fs';
+import path from 'path';
+import express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
@@ -14,10 +16,22 @@ async function bootstrap() {
   });
 
   app.use(helmet());
+  const swaggerUiPath = path.join(__dirname, '..', 'swagger-static');
+  app.use('/api/swagger-ui-standalone-preset.js', express.static(swaggerUiPath, {
+    setHeaders: (res, filePath) => {
+      res.type('application/javascript');
+    },
+  }));
 
+  app.use('/api/swagger-ui.css', express.static(swaggerUiPath, {
+    setHeaders: (res, filePath) => {
+      res.type('text/css');
+    },
+  }));
   setupSwagger(app);
   await app.listen(process.env.PORT);
   const serverUrl = await app.getUrl();
+
 
   console.log(`Server running at : ${serverUrl}`)
 }
